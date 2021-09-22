ea7a79a8704e5b49755f4ec0f4f96e9f5054e8e3
ZhaoXinsi <44218356+ZhaoXinsi@users.noreply.github.com>
2021-09-22 10:48:38 +0800

    Update create_oracle_schema.sql

--- sample-dbs/tpcc/ob_oracle/create_oracle_schema.sql
+++ sample-dbs/tpcc/ob_oracle/create_oracle_schema.sql
@@ -1,5 +1,8 @@
 -- run as user 'sys' in the tenant of type oracle
 create user tpcc identified by 123456;
-grant all privileges on tpcc.* to tpcc;
-grant select on sys.* to tpcc;
+grant connect,resource to tpcc;
+grant create table to tpcc;
+grant create view to tpcc;
+grant create procedure to tpcc;
+grant create type to tpcc;
 create tablegroup tpcc_group partition by hash partitions 6;
